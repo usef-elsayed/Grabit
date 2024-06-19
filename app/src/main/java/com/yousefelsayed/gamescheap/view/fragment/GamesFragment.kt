569b078c856b5fa2d030aafdd0f16d4f0bbf1c40
usef <yousefelsayed.dev@gmail.com>
2024-06-20 02:18:45 +0300

    Fixed search function

--- app/src/main/java/com/yousefelsayed/gamescheap/view/fragment/GamesFragment.kt
+++ app/src/main/java/com/yousefelsayed/gamescheap/view/fragment/GamesFragment.kt
@@ -112,7 +112,7 @@ class GamesFragment: Fragment() {
                     }
                     Status.LOADING -> {
                         showShimmerLayout()
-                    }
+                    } 
                 }
             }
         }
